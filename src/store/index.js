d9c50ae335d289a687c96e79f1884ad6b7a1a1c0
Alberto Blázquez <hola@albertoblazquez.net>
2016-06-14 23:31:22 +0200

    Warn when actions are unhandled. Use newer createStore API

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { applyMiddleware, createStore } from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
+import reduxUnhandledAction from 'redux-unhandled-action'
+
+const callback = (action) =>
+	console.warn(`${action.type} was unhandled or didn't lead to creation of a new state object`)
 
 const middlewares = localStorage.getItem('redux-logger')
- ? applyMiddleware(thunkMiddleware, createLogger())
- : applyMiddleware(thunkMiddleware)
+ ? applyMiddleware(thunkMiddleware, reduxUnhandledAction(callback), createLogger())
+ : applyMiddleware(thunkMiddleware, reduxUnhandledAction(callback))
 
 export const configureStore = (rootReducer, initialState) =>
-  middlewares(createStore)(rootReducer, initialState)
+  createStore(rootReducer, initialState, middlewares)
