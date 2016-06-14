a813629e444e48457e800ce5eaed9f4b38e2725c
Alberto Blázquez <hola@albertoblazquez.net>
2016-06-14 23:32:39 +0200

    Refactor root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,16 @@
 import { combineReducers } from 'redux'
 
-import chatHandlers from './ChatReducer'
-import gameHandlers from './GameReducer'
+import chat from './ChatReducer'
+import game from './GameReducer'
 
-function reducer (handlers, state, action) {
-  // '@@redux/INIT' or others are not handled
-  if (handlers[action.type]) {
-    return handlers[action.type](state, action)
-  } else if (localStorage.getItem('redux-logger')) {
-    console.warn('Unhandled action', action.type)
-  }
-
-  return state
-}
+const reducer = (handlers, state, action) =>
+  handlers[action.type] ? handlers[action.type](state, action) : state
 
 export default function createRootReducer (additionalReducers = {}) {
   const reducers = {
-    chat: chatHandlers(reducer),
-    game: gameHandlers(reducer),
+    chat: chat(reducer),
+    game: game(reducer),
   }
 
-  return combineReducers(Object.assign(additionalReducers, reducers))
+  return combineReducers(Object.assign({}, additionalReducers, reducers))
 }
