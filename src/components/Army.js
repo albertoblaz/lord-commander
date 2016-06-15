c86f207293bfc61eaec0d693b7bf907fb6d053c0
Alberto Blázquez <hola@albertoblazquez.net>
2016-06-16 01:49:31 +0200

    Improve Army view

--- src/components/Army.js
+++ src/components/Army.js
@@ -1,12 +1,18 @@
 import React, { PropTypes } from 'react'
 
 const Army = (props) =>
-  <span>{props.owner === 'albertoblaz' ? 'AB' : 'WZ'}</span>
+  <span
+    onClick={props.onClick.bind(this, props.id)}
+    style={{ backgroundColor: 'yellow', color: 'black' }}
+  >
+    {props.owner === 'albertoblaz' ? 'AB' : 'WZ'}
+  </span>
 
 Army.propTypes = {
   id: PropTypes.string.isRequired,
   owner: PropTypes.string.isRequired,
   provinceId: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 }
 
 export default Army
