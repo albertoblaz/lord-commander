d0481691ae3c3a7235a296bef74e326b9004f0ac
Alberto Blázquez <hola@albertoblazquez.net>
2016-06-16 01:49:06 +0200

    Remove commas. Add name to each army

--- src/reducers/ArmyReducer.js
+++ src/reducers/ArmyReducer.js
@@ -1,22 +1,25 @@
-import actions from '../actions/ArmyActionCreators'
+// import actions from '../actions/ArmyActionCreators'
 import gameActions from '../actions/GameActionCreators'
 
 const initialState = {
   armies: {
     '0': {
-      'id': '0',
-      'owner': 'albertoblaz',
-      'provinceId': '22',
+      id: '0',
+      owner: 'albertoblaz',
+      name: 'Grande Armée',
+      provinceId: '22',
     },
     '1': {
-      'id': '1',
-      'owner': 'albertoblaz',
-      'provinceId': '23',
+      id: '1',
+      owner: 'albertoblaz',
+      name: '2nd Republican Army',
+      provinceId: '23',
     },
     '2': {
-      'id': '2',
-      'owner': 'adriantom3',
-      'provinceId': '32',
+      id: '2',
+      owner: 'adriantom3',
+      name: 'Imperial Army',
+      provinceId: '32',
     },
   },
 }
