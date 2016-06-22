478ddee6c94a91d3544415af612ac98d5587463a
Alberto Blázquez <hola@albertoblazquez.net>
2016-06-23 01:31:00 +0200

    Introduce new Home component

---
+++ src/containers/Home.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Home = () =>
+  <span>Welcome to Lord Commander!</span>
+
+export default Home
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import App from './containers/App'
+import Home from './containers/Home'
 import Game from './containers/Game'
 import Stats from './containers/Stats'
 import Settings from './containers/Settings'
@@ -9,7 +10,8 @@ import Settings from './containers/Settings'
 const router = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Game}/>
+      <IndexRoute component={Home}/>
+      <Route path="game" component={Game}/>
       <Route path="stats" component={Stats}/>
       <Route path="settings" component={Settings}/>
     </Route>
