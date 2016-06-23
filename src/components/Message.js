d904871b1ee3a26e6e66da009e812e078173a528
Alberto Blázquez <hola@albertoblazquez.net>
2016-06-23 13:26:51 +0200

    fixup! static chat

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,19 @@
 import React, { PropTypes } from 'react'
 
+import { ListItem } from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble'
+
+const AVATAR_URL = 'https://avatars.slack-edge.com/2016-04-18/35512615170_341dd821da9224afd14f_512.jpg'
+
 const Message = (props) =>
-  <li>
-    <img src={props.img} />
-    <strong>{props.username}</strong>
-    <p>{props.text}</p>
-  </li>
+  <ListItem
+    leftAvatar={<Avatar src={AVATAR_URL} />}
+    primaryText={props.username}
+    secondaryText={props.text}
+    secondaryTextLines={1}
+    rightIcon={<CommunicationChatBubble />}
+  />
 
 Message.propTypes = {
   id: PropTypes.number.isRequired,
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,18 +1,33 @@
 import React, { PropTypes } from 'react'
-import _ from 'lodash'
+
+import Drawer from 'material-ui/Drawer'
+import { List } from 'material-ui/List'
+import Subheader from 'material-ui/Subheader'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 
 import Message from './Message'
 
 const Messages = (props) =>
-  <aside className="chat">
+  <Drawer open={true} openSecondary={true}>
     <div>
       <h2>Chat</h2>
-      <button onCLick={props.createNewChat}>+</button>
+      <FloatingActionButton
+        mini={true}
+        secondary={true}
+        onCLick={props.createNewChat}
+      >
+        <ContentAdd />
+      </FloatingActionButton>
     </div>
-    <ul>
-      {_.map(props.messages, (message) => <Message key={message.id} {...message} />)}
-    </ul>
-  </aside>
+
+    <List>
+      <Subheader>Recent chats</Subheader>
+      {props.messages.map((message, index) =>
+        <Message value={index} key={message.id} {...message} />
+      )}
+    </List>
+  </Drawer>
 
 Messages.propTypes = {
   messages: PropTypes.array.isRequired,
